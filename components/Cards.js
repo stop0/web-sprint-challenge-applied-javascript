c515388717585153da003fa0c3fdbc371e2005ac
MRSTOP0\Stop0 <dawodmohmmed@gmail.com>
2020-12-04 11:52:52 -0500

    Inital Commit

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,48 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+
+function article(Obj) {
+    const card = document.createElement("div");
+    const headline = document.createElement("div");
+    const author = document.createElement("div");
+    const imgCon = document.createElement("div");
+    const img = document.createElement("img");
+    const name = document.createElement("span");
+
+    card.appendChild(headline)
+    card.appendChild(author)
+    author.appendChild(imgCon)
+    imgCon.appendChild(img)
+    card.appendChild(author)
+    author.appendChild(name)
+
+    card.classList.add("card")
+    headline.classList.add("headline")
+    author.classList.add("author")
+    imgCon.classList.add("img-container")
+
+
+
+
+    headline.textContent = Obj.headline
+    img.src = Obj.authorPhoto
+    name.textContent = Obj.authorName
+
+
+    return card
+}
+
+
+
+
+axios.get("https://lambda-times-api.herokuapp.com/articles")
+    .then( response => {
+        console.log(response.data)
+        // topicL.append(topics(element))
+    // /  topicL.append(topics(response.data.topics))
+    })
+    .catch( err => {
+      console.log(err)
+    })
--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,29 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    const header = document.createElement("div");
+    const date = document.createElement("span");
+    const lambda = document.createElement("h1");
+    const temp = document.createElement("span");
+
+    header.appendChild(date)
+    header.appendChild(lambda)
+    header.appendChild(temp)
+
+    header.classList.add("header")
+    date.classList.add("date")
+    temp.classList.add("temp")
+
+    date.textContent = "MARCH 28, 2020"
+    lambda.textContent = "Lambda Times"
+    temp.textContent = "98°"
+
+
+    return header
+}
+
+
+let Dom = document.querySelector(".header-container")
+
+Dom.appendChild(Header())
